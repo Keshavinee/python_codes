bad984b925dc9316644132283171115db9ee9ba9
Keshavinee <62760584+Keshavinee@users.noreply.github.com>
2022-06-27 16:44:09 +0530

    Written in O(logn)

--- find_occ.py
+++ find_occ.py
@@ -1,15 +1,30 @@
 def findOccOf(L,x):
-    t=[]
-    f=False
-    for i in range(len(L)):
-        if L[i]==x:
-            if len(t)==0:
-                t.append(i)
-            f=True
+    low = 0
+    high = len(L)
+    while (low <= high):
+        mid = (low + high)//2
+        if L[mid]==x:
+            idx = mid
+            while idx >= low and L[idx]==x:
+                idx -= 1
+            if (idx < low and L[idx+1] == x) or (idx >= low):
+                a = idx + 1
+            
+            idx = mid
+            while idx <= high and L[idx]==x:
+                idx += 1
+            if (idx > low and L[idx-1] == x) or (idx <= high):
+                b = idx - 1
+            
+            return a,b
+
+        elif x > L[mid]:
+            low = mid + 1
+
         else:
-            if f:
-                return (t[0],i-1)
-    return (None,None)
+            high = mid - 1
+    return (None,None)        
+
 A = [int(item) for item in input().split(" ")]
 x = int(input())
 timeout = 1.0 # Timeout in sec
